94dd525ca95dceca29dcaf5e968837a608c31224
Guilherme Torres <guilhermetorres97@gmail.com>
2019-06-06 12:14:02 -0300

    source code started/touhou wiki uploaded

---
+++ src-analyzer/main.py
@@ -0,0 +1,8 @@
+import json
+import matplotlib
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    pass
---
+++ src-network-builder/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <curl/curl.h>
+#include "json.hpp"
+
+using json = nlohmann::json;
+const std::string output_file = "../touhou-network.json";
+
+int main()
+{
+    json links;
+
+
+
+    return 0;
+}
