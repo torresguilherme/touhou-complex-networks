7da25351375134d25f22d815f272251d2bacbdbb
Guilherme Torres <guilhermetorres97@gmail.com>
2019-06-07 10:20:34 -0300

    reading chars and building pre-network

--- network-builder/main.py
+++ network-builder/main.py
@@ -2,16 +2,51 @@ import html.parser as parser
 import json
 
 INDEX = '../touhou_wiki/en.touhouwiki.net/index.html'
+CHARACTERS_PAGE = '../touhou_wiki/en.touhouwiki.net/wiki/Category_Characters.html'
+MAIN_CHAR_1 = '../touhou_wiki/en.touhouwiki.net/wiki/Reimu_Hakurei.html'
+MAIN_CHAR_2 = '../touhou_wiki/en.touhouwiki.net/wiki/Marisa_Kirisame.html'
+
+network = {}
+links = []
+
+class CharactersFinder(parser.HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        global network
+        if tag == 'div':
+            if self.reading_chars:
+                self.reading_chars = False
+            for name, attr in attrs:
+                if name == 'class' and attr == 'mw-category-group':
+                    self.reading_chars = True
+        
+        if tag == 'a' and self.reading_chars:
+            for name, attr in attrs:
+                if name == 'href' and not 'Characters' in attr:
+                    links.append(attr)
+                elif name == 'title' and not 'Characters' in attr:
+                    network[attr] = {
+                        'dialogues': {},
+                        'fought': {},
+                        'mentions': {},
+                        'major_relationship_wiki': {},
+                        'minor_relationship_wiki': {}
+                    }
 
 def main():
-    # init file to be saved
-    graph = {}
-
     # parse init page
-    html_parser = parser.HTMLParser()
-    index_file = open(INDEX, 'r')
-    line_string = index_file.readlines()
-    parser.feed(line_string)
+    html_parser = CharactersFinder()
+    html_parser.reading_chars = False
+    html_parser.count = 0
+    characters_file = open(CHARACTERS_PAGE, 'r')
+    line_list = characters_file.readlines()
+    for line in line_list:
+        html_parser.feed(line)
+    
+    json_string = json.dumps(network)
+    with open('../network.json', 'w') as f:
+        f.write(json_string)
+    
+    return 0
 
 if __name__ == "__main__":
-    pass
+    main()
